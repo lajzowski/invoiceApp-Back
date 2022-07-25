12c9eb5cc368397ca136654973ed9dbf30f313ec
Marcin Woźnica <poczta@marcin.wroclaw.pl>
2022-07-25 19:54:12 +0200

    Added: helmet middleware and updated package.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  (app as NestExpressApplication).use(helmet());
   await app.listen(3001);
 }
 bootstrap();
